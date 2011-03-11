571e18217454db2469e11ae783fde549f2967bfc
Dustin Diaz <polvero@gmail.com>
2011-03-11 15:22:30 -0800

    add copyright file

---
+++ src/copyright.js
@@ -0,0 +1,7 @@
+/*!
+  * qwery.js - copyright @dedfat
+  * original inspiration: http://simonwillison.net/2003/Mar/25/getElementsBySelector/
+  * https://github.com/polvero/qwery
+  * Follow our software http://twitter.com/dedfat
+  * MIT License
+  */
