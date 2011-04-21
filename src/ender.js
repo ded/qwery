c098855b1beeaf6738bbf74b8a324a753d48a774
Dustin Diaz <polvero@gmail.com>
2011-04-21 10:01:15 -0700

    make find() return new collection and not alter existing collection

--- src/ender.js
+++ src/ender.js
@@ -10,15 +10,7 @@
           r.push(els[j]);
         }
       }
-      for (i = 0, l = this.length; i < l; i++) {
-        delete this[i];
-      }
-      this.elements = q.uniq(r);
-      this.length = this.elements.length;
-      for (i = 0, l = this.length; i < l; i++) {
-        this[i] = this.elements[i];
-      }
-      return this;
+      return $(q.uniq(r));
     }
   }, true);
 }();
