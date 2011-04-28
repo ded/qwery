24b25e01254890b57903d1b32e67be76426891a1
Dustin Diaz <polvero@gmail.com>
2011-04-27 23:00:35 -0700

    - remove closest. belongs in dom util - bump package

--- src/ender.js
+++ src/ender.js
@@ -1,19 +1,6 @@
 !function () {
   var q = qwery.noConflict();
   $._select = q;
-
-  var indexOf = Array.prototype.indexOf ?
-    function (ar, val) {
-      return ar.indexOf(val);
-    } :
-    function (ar, val) {
-      for (var i = 0; i < ar.length; i++) {
-        if ( ar[i] === val ) {
-          return i;
-        }
-      }
-      return -1;
-    };
   $.ender({
     find: function (s) {
       var r = [], i, l, j, k, els;
@@ -21,19 +8,6 @@
         els = q(s, this[i]);
         for (j = 0, k = els.length; j < k; j++) {
           r.push(els[j]);
-        }
-      }
-      return $(q.uniq(r));
-    },
-
-    closest: function (s) {
-      var r = [], i, l, p, els = q(s);
-      for (i = 0, l = this.length; i < l; i++) {
-        p = this[i];
-        while (p = p.parentNode) {
-          if (els.indexOf(p) != -1) {
-            r.push(p); break;
-          }
         }
       }
       return $(q.uniq(r));
