cf23504d7896dad6bfc400ae613cff2c84e2f259
Dustin Diaz <polvero@gmail.com>
2014-02-13 13:45:29 -0800

    try travis again

--- phantom.js
+++ phantom.js
@@ -1,20 +1,23 @@
-var page = require('webpage').create()
-page.open('http://localhost:3000/tests/', function() {
-
-  function f() {
-    setTimeout(function () {
-      var clsName = page.evaluate(function() {
-        var el = document.getElementById('tests')
-        return el.className
-      })
-      if (!clsName.match(/sink-done/)) f()
-      else {
-        var pass = !!clsName.match(/sink-pass/)
-        if (pass) console.log('tests have passed')
-        else console.log('tests failed')
-        phantom.exit(pass ? 0 : 1)
+var phantom = require('phantom')
+phantom.create(function (ph) {
+  return ph.createPage(function (page) {
+    return page.open('http://localhost:3000/tests/', function (status) {
+      function f() {
+        setTimeout(function () {
+          var clsName = page.evaluate(function() {
+            var el = document.getElementById('tests')
+            return el.className
+          })
+          if (!clsName.match(/sink-done/)) f()
+          else {
+            var pass = !!clsName.match(/sink-pass/)
+            if (pass) console.log('qwery tests have passed')
+            else console.log('qwery tests have failed')
+            phantom.exit(pass ? 0 : 1)
+          }
+        }, 100)
       }
-    }, 100)
-  }
-  f()
+      f()
+    })
+  })
 })
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 var cp = require('child_process')
   , spawn = cp.spawn
   , server  = spawn('./node_modules/serve/bin/serve', ['.'])
-  , phantom = spawn('./vendor/phantomjs', ['./phantom.js'])
+  , phantom = spawn('node', ['./phantom.js'])
 
 phantom.on('exit', function (code, signal) {
   var outcome = code == 0 ? 'passed' : 'failed'
-  console.log('Qwery tests have %s', outcome)
+  console.log('Qwery tests have %s', outcome, code)
   server.kill('SIGHUP')
   process.exit(code)
 })
