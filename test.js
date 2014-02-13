a48ed7263c0b1840a31514a9354c9cacf175f77b
Dustin Diaz <polvero@gmail.com>
2014-02-13 13:03:07 -0800

    use vendor/phantom

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var cp = require('child_process')
   , spawn = cp.spawn
   , server  = spawn('./node_modules/serve/bin/serve', ['.'])
-  , phantom = spawn('phantomjs', ['phantom.js'])
+  , phantom = spawn('./vendor/phantomjs', ['./phantom.js'])
 
 phantom.on('exit', function (code, signal) {
   var outcome = code == 0 ? 'passed' : 'failed'
